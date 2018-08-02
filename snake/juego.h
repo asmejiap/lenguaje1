5c85ead6959d8188dbde9f1f95b90d7731cdde00
asmejiap <40250120+asmejiap@users.noreply.github.com>
2018-08-01 19:56:00 -0600

    Add files via upload

---
+++ snake/juego.h
@@ -0,0 +1,12 @@
+void dificultad();
+void gotoxy();
+void pintar();
+void dibujarcuerpo();
+void guardarposicion();
+void borrarcuerpo();
+void Teclear();
+void comida();
+bool gameover();
+void puntosx();
+void proceso();
+
